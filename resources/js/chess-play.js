e90afe59d47e317d3a44963f86acc8a6f41a3c5a
tien.xuan.vo <tien.xuan.vo@gmail.com>
2014-11-07 17:04:03 +0700

    Highlight legal moves.

--- resources/js/chess-play.js
+++ resources/js/chess-play.js
@@ -6,27 +6,44 @@ else {
     chess = new Chess(chess_start_position);
 }
 
+var removeGreySquares = function() {
+    $('#board .square-55d63').css('background', '');
+};
+
+var greySquare = function(square) {
+    var squareEl = $('#board .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareEl.hasClass('black-3c85d') === true) {
+        background = '#696969';
+    }
+
+    squareEl.css('background', background);
+};
+
 // do not pick up pieces if the game is over
 // only pick up pieces for the side to move
 var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false;
-  }
+    if (game.game_over() === true ||
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
 };
 
 var onDrop = function(source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    // TODO: show a dialog for choosing promotion.
-    promotion: 'q'
-  });
+    removeGreySquares();
 
-  // illegal move
-  if (move === null) return 'snapback';
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        // TODO: show a dialog for choosing promotion.
+        promotion: 'q'
+    });
+
+    // illegal move
+    if (move === null) return 'snapback';
 
     // Highlight the move.
     var color = chess.turn() == 'b' ? 'white' : 'black';
@@ -44,7 +61,7 @@ var onDrop = function(source, target) {
         },
         success: function(data) {
             if (data.code == '200') {
-                // Do some things here.
+            // Do some things here.
             }
         },
         dataType: 'json',
@@ -52,10 +69,33 @@ var onDrop = function(source, target) {
     });
 };
 
+var onMouseoverSquare = function(square, piece) {
+    // get list of possible moves for this square
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    });
+
+    // exit if there are no moves available for this square
+    if (moves.length === 0) return;
+
+    // highlight the square they moused over
+    greySquare(square);
+
+    // highlight the possible squares for this piece
+    for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+    }
+};
+
+var onMouseoutSquare = function(square, piece) {
+    removeGreySquares();
+};
+
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 var onSnapEnd = function() {
-  board.position(game.fen());
+    board.position(game.fen());
 };
 
 var cfg = {
@@ -64,6 +104,8 @@ var cfg = {
     onDragStart: onDragStart,
     orientation: chess_orientation,
     onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
     onSnapEnd: onSnapEnd,
     pieceTheme: pieceTheme
 };
