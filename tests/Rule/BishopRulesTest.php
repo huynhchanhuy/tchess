7b23c884326f4347ab06e8b10525495e6c358520
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-18 08:52:51 +0700

    Improve bishop move diagonally rule test case.

--- tests/Rule/BishopRulesTest.php
+++ tests/Rule/BishopRulesTest.php
@@ -10,11 +10,16 @@ class BishopRulesTest extends UnitTestBase
 {
     public function testNotMoveDiagonally()
     {
+        /* @var $board Board */
         $board = $this->serializer->deserialize('rnbqkbnr/pppppp1p/6p1/8/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 2', 'Tchess\Entity\Board', 'fen');
-        $move = new Move($board, 'white', 'c1 h5');
 
+        $move = new Move($board, 'white', 'c1 f3');
         $errors = $this->validator->validate($move);
         $this->assertTrue(count($errors) > 0, 'Did not move diagonally');
+
+        $move = new Move($board, 'white', 'e1 d3');
+        $errors = $this->validator->validate($move);
+        $this->assertTrue(count($errors) > 0, 'Can not move like a knight');
     }
 
     public function testGoThroughAnotherPiece()
