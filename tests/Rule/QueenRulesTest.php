81fb0f77ddbba8b1e637d93808508c2928250a72
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-22 23:37:32 +0700

    Add valid queen moves test case

--- tests/Rule/QueenRulesTest.php
+++ tests/Rule/QueenRulesTest.php
@@ -18,4 +18,21 @@ class QueenRulesTest extends UnitTestBase
         $this->assertTrue(count($errors) > 0, 'Invalid queen move');
     }
 
+    public function testValidMove()
+    {
+        /* @var $board Board */
+        $board = $this->serializer->deserialize('rnbqkbnr/ppppppp1/7p/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2', 'Tchess\Entity\Board', 'fen');
+
+        $move = new Move($board, 'white', 'd1 g4');
+        $errors = $this->validator->validate($move);
+        $this->assertEquals(0, count($errors), 'Valid move');
+
+        /* @var $board Board */
+        $board = $this->serializer->deserialize('rnbqkbnr/pppppp2/6pp/8/3P4/3Q4/PPP1PPPP/RNB1KBNR w KQkq - 0 3', 'Tchess\Entity\Board', 'fen');
+
+        $move = new Move($board, 'white', 'd3 a3');
+        $errors = $this->validator->validate($move);
+        $this->assertEquals(0, count($errors), 'Valid move');
+    }
+
 }
