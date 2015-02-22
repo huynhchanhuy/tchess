d1028372cc030f96347e141b51e6a48ec7b7f08f
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-22 11:12:07 +0700

    Add move pawn backward test case.

---
+++ tests/Rule/PawnRulesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tchess\Tests\Rule;
+
+use Tchess\Tests\UnitTestBase;
+use Tchess\Entity\Piece\Move;
+use Tchess\Entity\Board;
+
+class PawnRulesTest extends UnitTestBase
+{
+    public function testMoveBackward()
+    {
+        /* @var $board Board */
+        $board = $this->serializer->deserialize('rnbqkbnr/1ppppppp/p7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 0 2', 'Tchess\Entity\Board', 'fen');
+
+        $move = new Move($board, 'white', 'a3 a2');
+        $errors = $this->validator->validate($move);
+        $this->assertTrue(count($errors) > 0, 'Can not move a pawn backward');
+    }
+
+}
