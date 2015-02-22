a1d8644135de452ddbb167fc0cde3a53440e1276
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-22 23:37:48 +0700

    Add valid rook move test case

--- tests/Rule/RookRulesTest.php
+++ tests/Rule/RookRulesTest.php
@@ -36,4 +36,14 @@ class RookRulesTest extends UnitTestBase
         $this->assertTrue(count($errors) > 0, 'Can not move through pieces');
     }
 
+    public function testValidMove()
+    {
+        /* @var $board Board */
+        $board = $this->serializer->deserialize('rnbqkbnr/pppppp2/6pp/8/7P/7R/PPPPPPP1/RNBQKBN1 w Qkq - 0 3', 'Tchess\Entity\Board', 'fen');
+
+        $move = new Move($board, 'white', 'h3 a3');
+        $errors = $this->validator->validate($move);
+        $this->assertEquals(0, count($errors), 'Valid move');
+    }
+
 }
