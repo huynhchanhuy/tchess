e5f414904a293ec0053e80181012e566dfbcfeaf
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-23 19:29:15 +0700

    Move room actions to room controller.

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -5,6 +5,10 @@ namespace Tchess\Controller;
 //use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Tchess\Helper\Paginator;
+use Tchess\RoomEvents;
+use Tchess\Event\RoomEvent;
+use Tchess\Entity\Player;
+use Tchess\Entity\Room;
 
 class RoomController extends BaseController
 {
@@ -39,6 +43,113 @@ class RoomController extends BaseController
             'paginator' => $paginator,
             'logged_in' => $this->isLoggedIn($request)
         ));
+    }
+
+    /**
+     * Join room.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return string
+     */
+    public function joinAction(Request $request, $room = null)
+    {
+        $em = $this->framework->getEntityManager();
+        $session = $request->getSession();
+        $sid = $session->getId();
+
+        $player = $em->getRepository('Tchess\Entity\Player')->findOneBy(array('sid' => $sid));
+
+        if (empty($player) || !$player instanceof Player) {
+            return $this->redirect($this->generateUrl('register'));
+        }
+
+        $joined_room = $player->getRoom();
+        if (!empty($joined_room) && $joined_room instanceof Room) {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
+        $room = $em->getRepository('Tchess\Entity\Room')->findOneBy(array('id' => $room));
+        $players = $room->getPlayers();
+
+        if (count($players) != 1) {
+            return $this->redirect($this->generateUrl('rooms'));
+        }
+
+        $opponent_player = current(reset($players));
+        $player->setColor($opponent_player->getColor() == 'white' ? 'black' : 'white');
+        $room->addPlayer($player);
+        $player->setRoom($room);
+        $em->flush();
+
+        $event = new RoomEvent($room, $player);
+        $this->framework->getEventDispatcher()->dispatch(RoomEvents::JOIN, $event);
+
+        return $this->redirect($this->generateUrl('homepage'));
+    }
+
+    /**
+     * Leave room.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return string
+     */
+    public function leaveAction(Request $request)
+    {
+        $em = $this->framework->getEntityManager();
+        $session = $request->getSession();
+        $sid = $session->getId();
+
+        $player = $em->getRepository('Tchess\Entity\Player')->findOneBy(array('sid' => $sid));
+
+        if (empty($player) || !$player instanceof Player) {
+            return $this->redirect($this->generateUrl('register'));
+        }
+
+        $room = $player->getRoom();
+        if (!empty($room) && $room instanceof Room) {
+            $em->remove($room->getGame());
+            $room->removePlayer($player);
+            $player->setRoom(null);
+            $em->flush();
+
+            $event = new RoomEvent($room, $player);
+            $this->framework->getEventDispatcher()->dispatch(RoomEvents::LEAVE, $event);
+        }
+
+        return $this->redirect($this->generateUrl('rooms'));
+    }
+
+    /**
+     * Create room.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return string
+     */
+    public function createAction(Request $request)
+    {
+        $em = $this->framework->getEntityManager();
+        $session = $request->getSession();
+        $sid = $session->getId();
+
+        $player = $em->getRepository('Tchess\Entity\Player')->findOneBy(array('sid' => $sid));
+
+        if (empty($player) || !$player instanceof Player) {
+            return $this->redirect($this->generateUrl('register'));
+        }
+
+        $joined_room = $player->getRoom();
+        if (empty($joined_room) || !$joined_room instanceof Room) {
+            $room = new Room();
+            $room->addPlayer($player);
+            $em->persist($room);
+            $player->setRoom($room);
+            $em->flush();
+
+            $event = new RoomEvent($room, $player);
+            $this->framework->getEventDispatcher()->dispatch(RoomEvents::CREATE, $event);
+        }
+
+        return $this->redirect($this->generateUrl('homepage'));
     }
 
 }
