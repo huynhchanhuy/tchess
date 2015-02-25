e952b68ec11dc17492d6a3c2b6dde30e2aa292e3
tien.xuan.vo <tien.xuan.vo@gmail.com>
2015-02-25 21:38:28 +0700

    Refactoring room controller test cases.

--- tests/Controller/RoomControllerTest.php
+++ tests/Controller/RoomControllerTest.php
@@ -9,15 +9,48 @@ use Tchess\Tests\WebTestBase;
  */
 class RoomControllerTest extends WebTestBase
 {
+    protected $player;
+
+    protected function setUp()
+    {
+        $this->player = static::createClient();
+        $crawler = $this->player->request('GET', '/register');
+        $form = $crawler->selectButton('register')->form(array(
+            'form[name]' => 'Player'
+        ));
+        $this->player->submit($form);
+    }
 
     /**
-     * Both players do not registered.
+     * Tests no room has been created.
      */
-    public function testNotRegister()
+    public function testNoRoomHasBeenCreated()
     {
-        $crawler = static::$client->request('GET', '/rooms');
-
+        $crawler = $this->player->request('GET', '/rooms');
         $this->assertEquals(0, $crawler->filter('a:contains("Watch")')->count());
+        $this->assertEquals(0, $crawler->filter('a:contains("Join")')->count());
+        $this->assertEquals(1, $crawler->filter('a:contains("Create room")')->count());
+        $this->assertEquals(0, $crawler->filter('a:contains("Go to your room")')->count());
+        $this->assertEquals(0, $crawler->filter('a:contains("Leave room")')->count());
+    }
+
+    /**
+     * Tests a room has been created.
+     */
+    public function testARoomHasBeenCreated()
+    {
+        // Create a room.
+        $crawler = $this->player->request('GET', '/rooms');
+        $link = $crawler->selectLink('Create room')->link();
+        $this->player->click($link);
+        $this->assertTrue($this->player->getResponse()->isRedirect('/'));
+
+        $crawler = $this->player->request('GET', '/rooms');
+        $this->assertEquals(1, $crawler->filter('a:contains("Go to your room")')->count());
+        $this->assertEquals(1, $crawler->filter('a:contains("Leave room")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Current Room")')->count());
+        $this->assertEquals(0, $crawler->filter('a:contains("Watch")')->count());
+        $this->assertEquals(0, $crawler->filter('a:contains("Join")')->count());
     }
 
 }
